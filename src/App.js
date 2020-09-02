93aa28ff0a68b3e5a7446f33c9ec1b0ebabc2e20
Prasant Prasath <33533156+5trang3@users.noreply.github.com>
2020-09-02 16:43:34 -0700

    Install Material-UI Search Bar and render it with App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import Container from '@material-ui/core/Container'
+import SearchBar from 'material-ui-search-bar'
 
 export default () => {
   return (
-    <div></div>
+    <Container>
+      <SearchBar/>
+    </Container>
   )
 };
