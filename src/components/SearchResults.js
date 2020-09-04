5cb986caa1a3c69f2337a755301826d4add44cb0
Prasant Prasath <33533156+5trang3@users.noreply.github.com>
2020-09-03 18:06:12 -0700

    Create and style SearchResults component.

---
+++ src/components/SearchResults.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/List';
+import ListItemText from '@material-ui/core/ListItemText';
+import IconButton from '@material-ui/core/IconButton';
+import AddIcon from '@material-ui/icons/Add';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+const superagent = require('superagent');
+
+const listItemStyles = makeStyles({
+  root: {
+    display: 'flex',
+    alignItems: 'flex-start',
+  }})
+const listItemTextStyles = makeStyles({
+  root: {
+    flexBasis: '300px',
+    flexGrow: '0'
+  }})
+const iconButtonStyles = makeStyles({
+  root: {
+    flexBasis: '48px',
+  }})
+
+export default (props) => {
+
+  const listItemClasses = listItemStyles();
+  const listItemTextClasses = listItemTextStyles();
+  const iconButtonClasses = iconButtonStyles();
+
+  let renderResults = () => props.results.map((result) => (
+    <ListItem dense className={ listItemClasses.root }>
+      <img class='search-image' src={ result.image } style={{ flexBasis: '50px', marginRight: '10px' }}></img>
+      <ListItemText primary={ result.title } secondary={ result.year } className={ listItemTextClasses.root } primaryTypographyProps={{ className: listItemTextClasses.primary }} secondaryTypographyProps={{ className: listItemTextClasses.secondary }}/>
+      <IconButton className={ iconButtonClasses.root }>
+        <AddIcon/>
+      </IconButton>
+    </ListItem>
+  ))
+
+  return (
+    <List>
+      { renderResults() }
+    </List>
+  )
+}
