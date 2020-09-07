f1109623cea43d2cb16616ba8c949a3e9c63bcde
Prasant <33533156+5trang3@users.noreply.github.com>
2020-09-07 16:52:02 -0700

    Add alt text for search result images.

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -40,7 +40,7 @@ export default (props) => {
   let renderResults = () => {
     return props.results.map(id => (
       <ListItem dense className={ listItemClasses.root } onMouseEnter={ () => props.setActive(id) } onMouseLeave={ () => props.setActive([]) }>
-        <img src={ props.movies[id].image } style={{ width: '50px', marginRight: '10px' }}></img>
+        <img src={ props.movies[id].image } style={{ width: '50px', marginRight: '10px' }} alt={ 'Movie poster for ' + props.movies[id].title }></img>
         <ListItemText primary={ props.movies[id].title } secondary={ props.movies[id].year } className={ listItemTextClasses.root } primaryTypographyProps={{ className: listItemTextClasses.primary }} secondaryTypographyProps={{ className: listItemTextClasses.secondary }}/>
         <IconButton className={ iconButtonClasses.root } onClick={ () => props.addMovie(id) } disabled={ props.isNominated(id) }>
           <AddIcon />
