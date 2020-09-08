40f48f4ce61c79db472388b0de1e2f5432799e39
Prasant Prasath <33533156+5trang3@users.noreply.github.com>
2020-09-07 18:19:18 -0700

    Display a title for the nominations display.

--- src/components/NominatedMoviesDisplay.js
+++ src/components/NominatedMoviesDisplay.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import NominatedMovie from './NominatedMovie.js'
 import '../styles/NominatedMoviesDisplay.css'
+import Typography from '@material-ui/core/Typography'
+
+import { makeStyles } from '@material-ui/core/Styles'
+
+const typographyStyles = makeStyles({
+  root: {
+    marginLeft: '5px',
+    textDecoration: 'underline'
+  }
+})
 
 export default (props) => {
+
+  const typographyClasses = typographyStyles();
 
   let renderNominatedMovies = () => {
     return props.nominations.map(id => <NominatedMovie nominatedMovie={ props.movies[id] } removeMovie={ props.removeMovie } id={ id } key={ id }/>)
   }
 
   return (
-    <div id='nominations-container'>
-      { renderNominatedMovies() }
+    <div>
+      { props.nominations.length === 0 ? null : <Typography variant='h6' className={ typographyClasses.root }>Your Nominations</Typography> }
+      <div id='nominations-container'>
+        { renderNominatedMovies() }
+      </div>
     </div>
   )
 }
